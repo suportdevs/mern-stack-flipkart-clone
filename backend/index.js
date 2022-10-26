c3bce727c240aca0369db9ed5d5fcba9fa7e8ede
suportdevs <suportdevs@gmail.com>
2022-10-26 09:08:47 +0600

    create mongodb and connect

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const env = require("dotenv");
 const bodyParser = require("body-parser");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 env.config();
+
+mongoose
+  .connect(process.env.DATABASE_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection successfull.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// request parsers
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser());
 
 app.listen(process.env.PORT, () => {
