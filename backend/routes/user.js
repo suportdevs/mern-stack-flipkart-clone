b20a1373d5736e8a299adcee4aa9e70fcf74c759
suportdevs <suportdevs@gmail.com>
2022-11-01 22:35:14 +0600

    work leave an other project

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,6 +7,7 @@ const authenticate = require("../middleware/authenticate");
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+
 router.post("/profile", authenticate, (req, res) => {
   res.status(200).json({ message: "authenticate" });
 });
